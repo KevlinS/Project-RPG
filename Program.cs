305598f9cb1e89de1a6cc9b1326a1db0b4265825
Kevlin <35543395+KevlinS@users.noreply.github.com>
2018-12-14 09:32:10 +0100

    Test Update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Github
+            // loic.janin@ynov.com
+            Console.WriteLine("UPDATE GITHUB");
+
+            new Menu();
+            Console.ReadLine();
+        }
+    }
+}
